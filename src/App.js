90426cf4bf96c9fcfb12cc407937c7d72b950a01
Ernur Kurmanbekov <erazavr85@gmail.com>
2020-01-23 19:03:32 +0600

    Added Routes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import Layout from "./components/Layout/Layout";
 import {Route, Switch} from "react-router";
 import DishesPage from "./containers/DishesPage/DishesPage";
 import OrderPage from "./containers/OrderPage/OrderPage";
+import NewDish from "./containers/NewDish/NewDish";
+import EditDish from "./containers/EditDish/EditDish";
 
 const App = () => (
     <Layout>
       <Switch>
         <Route path='/' exact component={DishesPage}/>
         <Route path='/orders' exact component={OrderPage}/>
+        <Route path='/newDish' exact component={NewDish}/>
+        <Route path='/editDish/:id' exact component={EditDish}/>
       </Switch>
     </Layout>
 );
