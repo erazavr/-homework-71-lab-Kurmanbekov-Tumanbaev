ef584387175563b26325f76f942406073d693c3f
Ernur Kurmanbekov <erazavr85@gmail.com>
2020-01-23 21:58:03 +0600

    Wrote a check if there is no picture

--- src/components/Dishes/Dishes.js
+++ src/components/Dishes/Dishes.js
@@ -7,7 +7,7 @@ const Dishes = props => {
         <div className='Dish'>
             <div className='Dishes-Info flex'>
                 <div>
-                    <img src={props.img} alt=""/>
+                    {props.dishes === '' ? <p>no img</p>: <img src={props.img} alt=""/>}
                 </div>
                 <p>{props.dishName}</p>
                 <p><b>{props.cost}KGS</b></p>
