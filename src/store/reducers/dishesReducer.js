96596036a04b2dbf26101591055ecd98bae1f04b
Ernur Kurmanbekov <erazavr85@gmail.com>
2020-01-24 11:29:29 +0600

    Added keys loading and error to state

--- src/store/reducers/dishesReducer.js
+++ src/store/reducers/dishesReducer.js
@@ -1,12 +1,19 @@
-import {DISHES_SUCCESS} from "../actions/actionsType";
+import {DISHES_FAILURE, DISHES_REQUEST, DISHES_SUCCESS} from "../actions/actionsType";
 
 const initialState = {
-    dishes: {}
+    dishes: {},
+    error: null,
+    loading: false
 };
 const dishesReducer = (state = initialState, action) => {
     switch (action.type) {
+
+        case DISHES_REQUEST:
+            return {...state, loading: true};
         case DISHES_SUCCESS:
-            return {...state, dishes: action.dish};
+            return {...state, dishes: action.dish,loading: false, error: null};
+        case DISHES_FAILURE:
+            return {...state, loading: false, error: action.error};
         default:
             return state
     }
