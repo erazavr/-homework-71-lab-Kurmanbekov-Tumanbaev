a7e8f7492f8c0831f4f37e6f0e2699127d5a8d06
Ernur Kurmanbekov <erazavr85@gmail.com>
2020-01-24 11:34:53 +0600

    Added spinner

--- src/containers/EditDish/EditDish.js
+++ src/containers/EditDish/EditDish.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {connect} from "react-redux";
 import {editDish, getDishes} from "../../store/actions/dishesActions";
 import '../NewDish/NewDish.css'
+import Spinner from "../../components/UI/Spinner/Spinner";
 class EditDish extends Component {
     state = {
         dishName: '',
@@ -31,47 +32,55 @@ class EditDish extends Component {
     };
     valueChanged = event => this.setState({[event.target.name]: event.target.value});
     render() {
+        let form = (
+            <form action="" onSubmit={this.editDish} className='form'>
+                <div className='form-inner'>
+                    <label htmlFor="" className='label'>Edit dish name</label>
+                    <input
+                        className='field'
+                        type="text"
+                        name='dishName'
+                        value={this.state.dishName}
+                        onChange={this.valueChanged}
+                    />
+                </div>
+                <div className='form-inner'>
+                    <label htmlFor="" className='label'>Edit cost</label>
+                    <input
+                        className='field'
+                        type="number"
+                        name='cost'
+                        value={this.state.cost}
+                        onChange={this.valueChanged}
+                    />
+                </div>
+                <div className='form-inner'>
+                    <label htmlFor="" className='label'>Edit img url</label>
+                    <input
+                        className='field'
+                        type="text"
+                        name='imgUrl'
+                        value={this.state.imgUrl}
+                        onChange={this.valueChanged}
+                    />
+                </div>
+                <button type='submit' className='form-btn'>Edit</button>
+            </form>
+        );
+        if (this.props.loading) {
+            form=<Spinner/>
+        }
         return (
             <div>
-                <form action="" onSubmit={this.editDish} className='form'>
-                    <div className='form-inner'>
-                        <label htmlFor="" className='label'>Edit dish name</label>
-                        <input
-                            className='field'
-                            type="text"
-                            name='dishName'
-                            value={this.state.dishName}
-                            onChange={this.valueChanged}
-                        />
-                    </div>
-                    <div className='form-inner'>
-                        <label htmlFor="" className='label'>Edit cost</label>
-                        <input
-                            className='field'
-                            type="number"
-                            name='cost'
-                            value={this.state.cost}
-                            onChange={this.valueChanged}
-                        />
-                    </div>
-                    <div className='form-inner'>
-                        <label htmlFor="" className='label'>Edit img url</label>
-                        <input
-                            className='field'
-                            type="text"
-                            name='imgUrl'
-                            value={this.state.imgUrl}
-                            onChange={this.valueChanged}
-                        />
-                    </div>
-                    <button type='submit' className='form-btn'>Edit</button>
-                </form>
+                {form}
             </div>
         );
     }
 }
 const mapStateToProps = state => ({
-    dishes: state.dishes.dishes
+    dishes: state.dishes.dishes,
+    loading: state.dishes.loading,
+    error: state.dishes.error
 });
 const mapDispatchToProps = dispatch => ({
     getDishes: () => dispatch(getDishes()),
